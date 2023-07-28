afd8fecf7e13e5d712be4204f88c524d205fc47f
rletilly <ronan.letilly@gmail.com>
2023-07-28 19:00:13 +0200

    Final

--- MyGreenStocks.py
+++ MyGreenStocks.py
@@ -54,6 +54,7 @@ def ESG_Score(stock_info):
         url = ("https://financialmodelingprep.com/api/v4/esg-environmental-social-governance-data-ratings?symbol="+stock_info[0][i]+"&apikey=963351a791575f888eed177dd9400e77")
         df2 = API_call(url)
         df2 = df2.drop(columns=["symbol","cik","companyName","industry","industryRank"], axis=1)
+        print(df2)
         ##################################################################################
         ################################# get Stock Price ################################
         url = ("https://financialmodelingprep.com/api/v3/quote-short/"+stock_info[0][i]+"?apikey=963351a791575f888eed177dd9400e77")
@@ -63,30 +64,29 @@ def ESG_Score(stock_info):
         agg_score.append(df)
         agg_grade.append(df2)
         agg_price.append(df3)
-    #################################### get Average grade ##############################    
+    #################################### get Average grade ##############################    len(stock_info) = 2
     for i in range(0, len(stock_info[0])):
         somme += (note.index(agg_grade[i]["ESGRiskRating"][0])+1) * stock_info[1][i] * agg_price[i]["price"][0]
         divider += stock_info[1][i]* agg_price[i]["price"][0]
-#A mettre dans une boxe en non pas dans le terminal --> A tester
-    messagebox.showwarning("Your results","La note globale, moyenne, pondérée la plus récente ("+str(agg_grade[0]["year"][0])+") est de: " + note[round(somme/divider)-1])
+    messagebox.showinfo("Your results","La note globale, moyenne, pondérée la plus récente ("+str(agg_grade[0]["year"][0])+") est de: " + note[round(somme/divider)-1])
     ################################### get Average score ###############################
-    for i in range(0, len(stock_info[0])):
-        for j in range (0,len(stock_info[0])):
-            ESG[i] += agg_score[j][str(df.columns[i])][0] * stock_info[1][j] * agg_price[j]["price"][0]
+    for i in range(0, len(stock_info[0])): 
+        for j in range(0, 3):#note 
+            ESG[j] += agg_score[i][str(df.columns[j])][0] * stock_info[1][i]* agg_price[i]["price"][0]
         divi += stock_info[1][i] * agg_price[i]["price"][0]
-#A mettre dans une boxe en non pas dans le terminal --> A tester 
-    messagebox.showwarning("Your results","La note "+ df.columns.values[0] +" moyenne, pondérée la plus récente ("+ str(agg_grade[0]["year"][0])+") est de: " + str(ESG[0]/divi)
-    +"\n"+"La note "+ df.columns.values[1] +" moyenne, pondérée la plus récente ("+ str(agg_grade[0]["year"][0])+") est de: " + str(ESG[1]/divi) +"\n"+
-    "La note "+ df.columns.values[2] +" moyenne, pondérée la plus récente ("+ str(agg_grade[0]["year"][0])+") est de: " + str(ESG[2]/divi)) 
 
-#Reads an excel file given by user that respects the topology --> A tester avec le call à L'API
+    messagebox.showinfo("Your results","La note "+ df.columns.values[0] +" moyenne la plus récente est de: " + str(ESG[0]/divi) +
+                        "\n\n"+"La note "+ df.columns.values[1] +" moyenne la plus récente est de: " + str(ESG[1]/divi) +
+                        "\n\n"+"La note "+ df.columns.values[2] +" moyenne la plus récente est de: " + str(ESG[2]/divi)) 
+
+#Reads an excel file given by user that respects the topology
 def read_excel(df):
     for i in range (0, len(df)):
         if df["ticker"][i]:
-            stock_list[0].append(df["ticker"][i])
+            stock_list[0].append(df["ticker"][i].upper())
         else:
             tik = get_companies_names(df["name"][i])
-            stock_list[0].append(tik["Companyname"][0]) # Vérifier que ce soit bien le nom 
+            stock_list[0].append(tik["Companyname"][0])
 
         if df["amount"][i] != 0 and not isinstance(df["amount"][i], str):
             stock_list[1].append(df["amount"][i])
